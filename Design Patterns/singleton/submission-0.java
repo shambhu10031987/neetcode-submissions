167e1cb1c24f802c63f4f9a52618d3b047bc960c
shambhu10031987 <34115499+shambhu10031987@users.noreply.github.com>
2026-08-13 07:34:58 +0530

    Add: singleton - submission-0

---
+++ Design Patterns/singleton/submission-0.java
@@ -0,0 +1,26 @@
+static class Singleton {
+        private static Singleton instance;
+        private String value;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if(instance==null){
+            instance=new Singleton();
+        }
+        return instance;
+    }
+
+    public String getValue() {
+        return value;
+
+    }
+
+    public void setValue(String value) {
+        instance.value=value;
+
+    }
+    
+}

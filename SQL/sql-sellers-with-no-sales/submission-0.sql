da1c660613c71acdcefb588a13c02acb1c49228d
shambhu10031987 <34115499+shambhu10031987@users.noreply.github.com>
2026-08-13 22:08:46 +0530

    Add: sql-sellers-with-no-sales - submission-0

---
+++ SQL/sql-sellers-with-no-sales/submission-0.sql
@@ -0,0 +1,2 @@
+-- Write your query below
+select s.seller_name from seller s left join orders o  on o.seller_id=s.seller_id and o.sale_date >='2020-01-01' and o.sale_date<'2021-01-01' where o.order_id is null order by s.seller_name asc;

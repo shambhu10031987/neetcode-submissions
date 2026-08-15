74aa977999d8802bab9b39dd7e845f6d2b2da8c5
shambhu10031987 <34115499+shambhu10031987@users.noreply.github.com>
2026-08-15 16:00:19 +0530

    Update: sql-customer-order-frequency - submission-0

--- SQL/sql-customer-order-frequency/submission-0.sql
+++ SQL/sql-customer-order-frequency/submission-0.sql
@@ -1,4 +1,15 @@
 -- Write your query below
+select c.customer_id ,c.name from 
+ orders o join product p on 
+o.product_id=p.product_id
+join
+customers c on                                                        c.customer_id=o.customer_id
+group by c.customer_id ,c.name
+ having sum(
+    case when o.order_date>='2020-06-01' and o.order_date<='2020-06-30' then o.quantity*p.price else 0 end)>=100
+and
+ sum(
+    case when o.order_date>='2020-07-01' and o.order_date<='2020-07-31' then o.quantity*p.price else 0 end)>=100
 
-select actor_id,director_id from actor_director group by actor_id,director_id 
-having count(*)>=3
+
+

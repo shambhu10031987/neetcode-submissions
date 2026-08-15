08c627ca4db4d3b428ce0d7a73f09991dd0e7253
shambhu10031987 <34115499+shambhu10031987@users.noreply.github.com>
2026-08-15 11:43:04 +0530

    Update: sql-bank-account-summary-ii - submission-0

--- SQL/sql-bank-account-summary-ii/submission-0.sql
+++ SQL/sql-bank-account-summary-ii/submission-0.sql
@@ -1,5 +1,3 @@
 -- Write your query below
-
-select Least(from_id,to_id) as person1, Greatest(from_id,to_id) as person2 ,count(*)as call_count ,sum(duration)as total_duration
-from calls  group by  LEAST(from_id, to_id),
-    GREATEST(from_id, to_id);
+select u.name ,sum(t.amount) as balance from users u join transactions t on u.account=t.account
+group by u.account having sum(t.amount)>10000 

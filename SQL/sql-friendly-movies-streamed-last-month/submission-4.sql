59adb0bf83f92966330f6d36f6e91ecc1186289f
shambhu10031987 <34115499+shambhu10031987@users.noreply.github.com>
2026-08-15 16:52:46 +0530

    Add: sql-friendly-movies-streamed-last-month - submission-4

---
+++ SQL/sql-friendly-movies-streamed-last-month/submission-4.sql
@@ -0,0 +1,7 @@
+-- Write your query below
+
+select distinct c.title from content c
+join tv_program t on c.content_id=t.content_id
+where t.program_date like '2020-06%' and
+c.kids_content='Y'
+and c.content_type='Movies'

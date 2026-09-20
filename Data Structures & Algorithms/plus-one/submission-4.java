310878318249947d09c36b769ae2f37ca9066f7c
shambhu10031987 <34115499+shambhu10031987@users.noreply.github.com>
2026-09-20 11:36:26 +0530

    Add: plus-one - submission-4

---
+++ Data Structures & Algorithms/plus-one/submission-4.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+            
+        }
+        //handling 9999 case
+        int p[]=new  int[digits.length+1];
+        p[0]=1;
+        return p;
+
+    }
+}
